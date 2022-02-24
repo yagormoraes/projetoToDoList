1836ac951408b0393c9e266b56f5fb2160a7eb8c
yagormorares <yago.dmoraes@gmail.com>
2022-02-23 21:06:35 -0300

    instalação do nodemon e usando o POST

--- src/controller/tarefaController.js
+++ src/controller/tarefaController.js
@@ -1,7 +1,9 @@
 const tarefaController = (app)=>{
     app.get('/tarefa', (req, res)=>{
-    res.send('Rota GET para entidade tarefa')
-})
+        //res.send('Rota GET para entidade tarefa')
+        res.send('Rastreamento da aplicação sendo feito com nodemon para a tarefa')
+    })
+    
 }
 
 export default tarefaController
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,8 +1,20 @@
 const userController = (app)=>{
-        app.get('/usuario', (req, res)=>{
-        res.send('Rota GET para entidade usuário')
+    app.get('/usuario', (req, res)=>{
+        //res.send('Rota GET para entidade usuário')
+        res.send('Rastreamento da aplicação sendo feito com nodemon')
     })
+
+    app.post('/usuario',(req,res)=>{
+        res.json({
+            "nome" : "yago",
+            "msg" : "fala fiote"
+        }
+
+        )
+    })
+    
 }
+
 
 export default userController
 
